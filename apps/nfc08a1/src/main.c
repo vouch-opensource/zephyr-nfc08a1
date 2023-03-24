43ec81021a1beade9451a5c24ba1951703fc1615
Mike Fikes <mike@fikesfarm.com>
2023-03-24 13:35:05 -0400

    Logging

--- apps/nfc08a1/src/main.c
+++ apps/nfc08a1/src/main.c
@@ -1,9 +1,12 @@
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_DECLARE(ST25R);
 
 int main()
 {
     while (1) {
-        printk("main loop\n");
+        LOG_INF("main loop");
         k_sleep(K_MSEC(5000));
 
     }
