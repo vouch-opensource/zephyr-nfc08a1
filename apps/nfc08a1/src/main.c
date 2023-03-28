ce51fbee4655b203f153a96abdfaa7f9a7f0b203
Mike Fikes <mike@fikesfarm.com>
2023-03-28 13:51:56 -0400

    Init RFAL

--- apps/nfc08a1/src/main.c
+++ apps/nfc08a1/src/main.c
@@ -1,10 +1,18 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
+#include "rfal_nfc.h"
+
 LOG_MODULE_DECLARE(ST25R);
 
 int main()
 {
+
+    ReturnCode rc = rfalNfcInitialize();
+    if (rc != ERR_NONE) {
+        LOG_ERR("RFAL init failed");
+    }
+
     while (1) {
         LOG_INF("main loop");
         k_sleep(K_MSEC(5000));
